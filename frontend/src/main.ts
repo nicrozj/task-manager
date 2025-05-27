f87100222c0c9bc358bafc10bcc7a035c808fd98
Igor Zhorkin <zhorkinigor@yandex.ru>
2025-05-27 23:53:07 +0300

    added pages

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,18 @@
 import { createApp } from "vue";
 import { createRouter, createWebHistory, RouterView } from "vue-router";
 import Index from "./pages/index.vue";
+import login from "./pages/login.vue";
+import registration from "./pages/registration.vue";
 
 import "./main.css";
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [{ path: "/", component: Index }],
+  routes: [
+    { path: "/", component: Index },
+    { path: "/registration", component: registration },
+    { path: "/login", component: login },
+  ],
 });
 
 createApp(RouterView).use(router).mount("body");
