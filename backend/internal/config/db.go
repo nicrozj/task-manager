2d75cad276479c62b2baeee21fa4f0b9e9e5d78f
Igor Zhorkin <zhorkinigor@yandex.ru>
2025-06-02 20:46:04 +0300

    Update db.go

--- backend/internal/config/db.go
+++ backend/internal/config/db.go
@@ -13,6 +13,7 @@ import (
 var migrationFS embed.FS
 
 func NewDBClient() (*sqlx.DB, error) {
+	fmt.Println(Envs.DB_URL)
 	db, err := sqlx.Connect("postgres", Envs.DB_URL)
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
